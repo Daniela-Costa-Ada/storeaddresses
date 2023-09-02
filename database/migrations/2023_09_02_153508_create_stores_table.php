b77eba8b53855892e27b5f4000793971835e662e
Daniela Costa <siedanipereira@gmail.com>
2023-09-02 15:26:58 -0300

    :sparkles: feat: create tables in the database

--- database/migrations/2023_09_02_153508_create_stores_table.php
+++ database/migrations/2023_09_02_153508_create_stores_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('stores', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->bigIncrements('id');
+            $table->string('name', 200);
             $table->timestamps();
         });
     }
---
+++ database/migrations/2023_09_02_165504_create_addresses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('foreign_id')->unique(); 
+            //$table->string('foreign_table', 200)->unique();
+            $table->string('postal_code');
+            $table->string('state');
+            $table->string('city', 200);
+            $table->string('sublocality', 200);
+            $table->string('street', 200);
+            $table->string('street_number', 200);
+            $table->string('complement', 200)->default('');
+
+            $table->foreign('foreign_id')->references('id')->on('stores')->onDelete('CASCADE')->onUpdate()->on('CASCADE');
+        
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
